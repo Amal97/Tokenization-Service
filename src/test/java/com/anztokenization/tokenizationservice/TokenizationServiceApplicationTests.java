c2501259c440c5bd1c5426221e29ac8d74917d26
Amal Chandra <amalchandra97@hotmail.com>
2026-02-10 21:03:35 +1300

    Initial Spring Boot project

---
+++ src/test/java/com/anztokenization/tokenizationservice/TokenizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anztokenization.tokenizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
